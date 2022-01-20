b61f3640d526edae7c1f0ad5ed5a59cda0ddc38e
tmay02 <97641235+tmay02@users.noreply.github.com>
2022-01-19 16:12:31 -0800

    Update MarkdownParse.java
    Only finds files with '.' in then

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,7 +15,9 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(markdown.substring(openParen + 1, closeParen.contains(".")){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
