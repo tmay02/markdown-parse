85f102000fc7339f372784a8a5e6bdf76d72639d
tmay02 <97641235+tmay02@users.noreply.github.com>
2022-01-26 16:51:03 -0800

    better tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
 import org.junit.*;
+import java.util.*;
 
 public class MarkdownParseTest {
     @Test
@@ -8,7 +12,10 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void TestMarkDownParse() {
-        //assertEquals()
+    public void testMarkownParse() throws IOException {
+        Path fileName = Path.of("/Users/tylermay/Documents/GitHub/markdown-parse/test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"),
+            MarkdownParse.getLinks(contents));
     }
 }
